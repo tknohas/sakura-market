fd17cc33d8e9fd39839d35900fbd22ccfbc23188
Masashi Takahashi <0xv17851726325v@gmail.com>
2024-11-15 16:42:26 +0900

    インデント修正

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,58 +5,58 @@ RSpec.describe Product, type: :model do
     expect(product).to be_valid
   end
 
-   describe 'バリデーションエラー' do
-     describe '商品名' do
-       it '入力なし' do
-         product.name = nil
-         expect(product).to be_invalid
-       end
+  describe 'バリデーションエラー' do
+    describe '商品名' do
+      it '入力なし' do
+        product.name = nil
+        expect(product).to be_invalid
+      end
 
-       it '文字数が多い' do
-         product.name = 'a' * 51
-         expect(product).to be_invalid
-       end
-     end
+      it '文字数が多い' do
+        product.name = 'a' * 51
+        expect(product).to be_invalid
+      end
+    end
 
-     describe '価格' do
-       it '入力なし' do
-         product.price = nil
-         expect(product).to be_invalid
-       end
+    describe '価格' do
+      it '入力なし' do
+        product.price = nil
+        expect(product).to be_invalid
+      end
 
-       it '負の値を入力' do
-         product.price = -1
-         expect(product).to be_invalid
-       end
+      it '負の値を入力' do
+        product.price = -1
+        expect(product).to be_invalid
+      end
 
-       it '数値以外を入力' do
-         product.price = 'あ'
-         expect(product).to be_invalid
-       end
-     end
+      it '数値以外を入力' do
+        product.price = 'あ'
+        expect(product).to be_invalid
+      end
+    end
 
-     describe '商品説明' do
-       it '入力なし' do
-         product.name = nil
-         expect(product).to be_invalid
-       end
+    describe '商品説明' do
+      it '入力なし' do
+        product.name = nil
+        expect(product).to be_invalid
+      end
 
-       it '文字数が多い' do
-         product.name = 'a' * 501
-         expect(product).to be_invalid
-       end
-     end
+      it '文字数が多い' do
+        product.name = 'a' * 501
+        expect(product).to be_invalid
+      end
+    end
 
-     describe '表示順' do
-       it '負の値を入力' do
-         product.price = -1
-         expect(product).to be_invalid
-       end
+    describe '表示順' do
+      it '負の値を入力' do
+        product.price = -1
+        expect(product).to be_invalid
+      end
 
-       it '数値以外を入力' do
-         product.price = 'あ'
-         expect(product).to be_invalid
-       end
-     end
-   end
+      it '数値以外を入力' do
+        product.price = 'あ'
+        expect(product).to be_invalid
+      end
+    end
+  end
 end
