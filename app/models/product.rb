a047ab62010e2d935df267f4e68d46ed03b37e88
Masashi Takahashi <0xv17851726325v@gmail.com>
2024-11-18 11:28:03 +0900

    商品画像サイズ指定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,11 @@
 class Product < ApplicationRecord
   acts_as_list
 
-  has_one_attached :image
+  has_one_attached :image do |attachable|
+    attachable.variant :small, resize_to_limit: [120, 80], preprocessed: true
+    attachable.variant :middle, resize_to_limit: [330, 219], preprocessed: true
+    attachable.variant :large, resize_to_limit: [500, 333], preprocessed: true
+  end
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
