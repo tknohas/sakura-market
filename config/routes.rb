6b01adf170c0a13c805e9a6958358e37ca08b537
Masashi Takahashi <0xv17851726325v@gmail.com>
2024-11-18 18:11:47 +0900

    ユーザーが新規登録できるように

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
   devise_for :admins, controllers: { sessions: 'admins/sessions' }
   namespace :admins do
     root 'products#index'
@@ -7,4 +8,8 @@ Rails.application.routes.draw do
       resource :publish, only: %i[update destroy], module: :products
     end
   end
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
 end
