a5f319721eaa0713546abd22ac9e5c08abae82a4
Masashi Takahashi <0xv17851726325v@gmail.com>
2024-11-18 15:05:33 +0900

    リファクタ

--- app/controllers/admins/products/publishes_controller.rb
+++ app/controllers/admins/products/publishes_controller.rb
@@ -1,11 +1,7 @@
 class Admins::Products::PublishesController < Admins::ApplicationController
   def update
     @product = Product.find(params[:product_id])
-    if @product.update(published_at: Time.current)
-      redirect_to admins_product_path(@product), notice: '公開しました'
-    else
-      flash_now[:alert] = '公開に失敗しました'
-      redirect_to edit_admins_product_path(@product)
-    end
+    @product.update!(published_at: Time.current)
+    redirect_to admins_product_path(@product), notice: '公開しました'
   end
 end
