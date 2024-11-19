35b7502719a19f5df40aff54b98e2749175bf35d
Masashi Takahashi <0xv17851726325v@gmail.com>
2024-11-19 13:50:22 +0900

    ユーザーが 商品一覧を閲覧できる

---
+++ spec/system/products_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe 'Products', type: :system do
+  let!(:published_product) { create(:product, name: 'にんじん', price: 1_000, description: '商品説明です。', position: 1, published_at: Time.current) }
+  let!(:unpublished_product) { create(:product, name: 'ピーマン', price: 2_000, description: '商品説明です。', position: 2, published_at: nil) }
+
+  describe '商品一覧' do
+    it '公開設定されている商品情報が表示される' do
+      visit products_path
+
+      expect(page).to have_css 'h2', text: '商品一覧'
+      expect(page).to have_content 'にんじん'
+      expect(page).to have_content '1,000円'
+      expect(page).to have_css 'img.product-image'
+      expect(page).to_not have_content 'ピーマン'
+      expect(page).to_not have_content '2,000円'
+    end
+  end
+end
