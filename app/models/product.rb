1ab97c81b18a317a84f1d98b906056d4099b3b5a
Masashi Takahashi <0xv17851726325v@gmail.com>
2024-11-20 09:28:12 +0900

    simple_formに変更

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,7 +11,7 @@ class Product < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :description, presence: true, length: { maximum: 200 }
-  validates :position, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :position, numericality: { only_integer: true, allow_nil: true, greater_than_or_equal_to: 0 }
 
   scope :order_by_position, -> { order(:position) }
   scope :published, -> { where.not(published_at: nil) }
