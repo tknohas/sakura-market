a3a36fe150a96e2c8c8bea3d9c8e777c96856b50
Masashi Takahashi <0xv17851726325v@gmail.com>
2024-12-22 13:26:45 +0900

    送料、代引き手数料、消費税が含まれた金額を計算できるように

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,6 +3,10 @@ class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items
 
+  TAX_RATE = 1.1
+  BASE_SHIPPING_FEE = 600
+  ITEM_PER_TIER = 5
+
   def merge_cart(session_cart)
     return unless session_cart
 
@@ -13,5 +17,38 @@ class Cart < ApplicationRecord
         existing_item.save!
       end
     end
+  end
+
+  def subtotal
+    cart_items.sum(&:total_price)
+  end
+
+  def cash_on_delivery_fee
+    case subtotal
+    when 0...10000
+      300
+    when 10000...30000
+      400
+    when 30000...100000
+      600
+    else
+      1000
+    end
+  end
+
+  def calculate_shipping_fee
+    BASE_SHIPPING_FEE * (cart_items.sum(:quantity) / ITEM_PER_TIER.to_f).ceil
+  end
+
+  def total_exclude_tax
+    subtotal + cash_on_delivery_fee + calculate_shipping_fee
+  end
+
+  def total_price
+    (total_exclude_tax * TAX_RATE).round
+  end
+
+  def tax_price
+    total_price - total_exclude_tax
   end
 end
